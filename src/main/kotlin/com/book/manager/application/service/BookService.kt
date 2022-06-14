ad16cad83805676fcc86eaac7664d31ed57ffb25
Yuta <y.sugiyama54@gmail.com>
2022-06-14 22:01:25 +0900

    アノテーションの追加

--- src/main/kotlin/com/book/manager/application/service/BookService.kt
+++ src/main/kotlin/com/book/manager/application/service/BookService.kt
@@ -2,7 +2,9 @@ package com.book.manager.application.service
 
 import com.book.manager.domain.model.BookWithRental
 import com.book.manager.domain.repository.BookRepository
+import org.springframework.stereotype.Service
 
+@Service
 class BookService(
     private val bookRepository: BookRepository
 ) {
